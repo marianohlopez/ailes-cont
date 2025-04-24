b2559a29571447add7ba34e8bea5840c48993a9f
mlopez <ml.3012@gmail.com>
2025-04-24 17:16:56 -0300

    host integer

--- fact.py
+++ fact.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 # --- CONFIGURACIÓN ---
 DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
+DB_PORT = int(os.getenv('DB_PORT'))
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 DB_NAME = os.getenv('DB_NAME')
